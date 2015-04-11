97cb90f5f052bb079523db1a495a11d4495dd9e6
Camden Voigt <jacksonvoigt@yahoo.com>
2015-04-11 11:13:22 -0500

    Added lots to Uno

--- src/unoTestGame/UnoPlayer.java
+++ src/unoTestGame/UnoPlayer.java
@@ -1,5 +1,22 @@
 package unoTestGame;
 
+import gameSim.*; 
+
 public class UnoPlayer {
 
+	public Hand playerHand;
+	private String name; 
+	
+	public UnoPlayer(){
+		name = "Alice";
+		playerHand = new Hand();
+	}
+	
+	public UnoPlayer(String name){
+		this.name = name; 
+	}
+	
+	public String getName(){
+		return name; 
+	}
 }
