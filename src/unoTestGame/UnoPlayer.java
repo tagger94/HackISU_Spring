58422e1f5ee1b9df84571f0f0ac8669909fef340
Camden Voigt <jacksonvoigt@yahoo.com>
2015-04-11 11:57:23 -0500

    more work on UNO

--- src/unoTestGame/UnoPlayer.java
+++ src/unoTestGame/UnoPlayer.java
@@ -4,12 +4,12 @@ import gameSim.*;
 
 public class UnoPlayer {
 
-	public Hand playerHand;
+	public Hand<UnoCard> playerHand;
 	private String name; 
 	
 	public UnoPlayer(){
 		name = "Alice";
-		playerHand = new Hand();
+		playerHand = new Hand<UnoCard>();
 	}
 	
 	public UnoPlayer(String name){
@@ -18,5 +18,49 @@ public class UnoPlayer {
 	
 	public String getName(){
 		return name; 
+	}
+	
+	public int findNumRed(){
+		int numRed = 0;
+		UnoCard red = new UnoCard(Color.RED, 1); 
+		for(int i = 0; i < playerHand.hand.size(); i++){
+			if(playerHand.hand.get(i).equalColor(red)){
+				numRed++; 
+			}
+		}
+		return numRed;
+	}
+	
+	public int findNumBlue(){
+		int numBlue = 0;
+		UnoCard blue = new UnoCard(Color.RED, 1); 
+		for(int i = 0; i < playerHand.hand.size(); i++){
+			if(playerHand.hand.get(i).equalColor(blue)){
+				numBlue++; 
+			}
+		}
+		return numBlue;
+	}
+	
+	public int findNumGreen(){
+		int numGreen = 0;
+		UnoCard green = new UnoCard(Color.RED, 1); 
+		for(int i = 0; i < playerHand.hand.size(); i++){
+			if(playerHand.hand.get(i).equalColor(green)){
+				numGreen++; 
+			}
+		}
+		return numGreen;
+	}
+	
+	public int findNumYellow(){
+		int numYellow = 0;
+		UnoCard yellow = new UnoCard(Color.RED, 1); 
+		for(int i = 0; i < playerHand.hand.size(); i++){
+			if(playerHand.hand.get(i).equalColor(yellow)){
+				numYellow++; 
+			}
+		}
+		return numYellow;
 	}
 }
