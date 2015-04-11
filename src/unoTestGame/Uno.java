ede9a47b9933d336a9ca3388eef4a34f4e49e3d1
Camden Voigt <jacksonvoigt@yahoo.com>
2015-04-11 13:03:33 -0500

    Started work on main method

--- src/unoTestGame/Uno.java
+++ src/unoTestGame/Uno.java
@@ -1,10 +1,48 @@
 package unoTestGame;
 
+import java.util.ArrayList;
+import gameSim.*; 
+
 public class Uno {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		UnoPlayerAI Bob = new UnoPlayerAI("Bob");
+		UnoPlayerAI Alice = new UnoPlayerAI("Alice");
+		
+		ArrayList<UnoCard> tempD = new ArrayList<UnoCard>();
+		createCards(tempD); 
+		Deck<UnoCard> deck = new Deck(tempD); 
 
+		
 	}
-
+	
+	public static void createCards(ArrayList<UnoCard> d){
+		
+		UnoCard red;
+		UnoCard blue;
+		UnoCard green; 
+		UnoCard yellow;
+		
+		for(int i = 0; i < 9; i++){
+			red = new UnoCard(Color.RED, i);
+			blue = new UnoCard(Color.BLUE, i);
+			green = new UnoCard(Color.GREEN, i);
+			yellow = new UnoCard(Color.YELLOW, i);
+			
+			if(i == 0){
+				d.add(red);
+				d.add(blue);
+				d.add(green);
+				d.add(yellow);
+			}
+			else{
+				for(int j = 1; j < 3; j++){
+					d.add(red);
+					d.add(blue);
+					d.add(green);
+					d.add(yellow);
+				}
+			}
+		}
+	}
 }
