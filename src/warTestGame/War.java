5bc8f76deeda1ed017c5b4a3db279ae516fbe713
Hawbaker <brennynh@iastate.edu>
2015-04-11 12:56:35 -0500

    War edits

--- src/warTestGame/War.java
+++ src/warTestGame/War.java
@@ -1,5 +1,30 @@
 package warTestGame;
 
+import exampleClasses.StandardPlayingCard;
+import gameSim.Card;
+import gameSim.Deck;
+
 public class War {
+
+	private Deck<Card> deck;
+
+	/**
+	 * Default new game constructor
+	 */
+
+	public War() {
+		WarPlayer player_1 = new WarPlayer("Player_1");
+		WarPlayer player_2 = new WarPlayer("Player_2");
+		deck = new Deck<Card>(StandardPlayingCard.makeDeck());
+		deck.shuffle();
+		
+		for(int a = 0; a < 26; a++){
+			player_1.drawCard(deck.draw());
+			player_2.drawCard(deck.draw());
+		}
+		
+		
+		
+	}
 
 }
--- src/warTestGame/WarPlayer.java
+++ src/warTestGame/WarPlayer.java
@@ -1,19 +1,75 @@
 package warTestGame;
 
+import gameSim.Card;
 import gameSim.Deck;
 import gameSim.Hand;
 
 /**
- * Designs players with a hand and discard pile
+ * Designs player for War with a name, hand, and discard pile
  * 
  * @author Brennyn Hawbaker
  *
  */
 public class WarPlayer {
-	
-	private Hand hand;
-	private Deck discardPile;
-	
-	
 
+	private String title;
+	private Hand<Card> hand;
+	private Deck<Card> discard;
+
+	/**
+	 * Default constructor
+	 */
+	public WarPlayer() {
+		title = "Player";
+		hand = new Hand<Card>();
+		discard = new Deck<Card>();
+	}
+
+	/**
+	 * Constructor with name
+	 * 
+	 * @param Name of player
+	 */
+	public WarPlayer(String title) {
+		this.title = title;
+		hand = new Hand<Card>();
+		discard = new Deck<Card>();
+	}
+	
+	/**
+	 * Adds card to player's hand
+	 */
+	public void drawCard(Card card){
+		
+	}
+	
+	
+	
+	//All getters and setters
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Hand<Card> getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand<Card> hand) {
+		this.hand = hand;
+	}
+
+	public Deck<Card> getDiscard() {
+		return discard;
+	}
+
+	public void setDiscard(Deck<Card> discard) {
+		this.discard = discard;
+	}
+	
+	
+	
 }
