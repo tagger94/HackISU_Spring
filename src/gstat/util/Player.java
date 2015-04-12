427aa52f110602a6a89a364b385dec584bc4751e
Onyszczak <mikeonys@iastate.edu>
2015-04-12 01:43:46 -0500

    Refined Card, Hand, Inventory, and Player

--- src/gstat/util/Player.java
+++ src/gstat/util/Player.java
@@ -2,11 +2,55 @@ package gstat.util;
 
 public interface Player {
 
+	/**
+	 * Increases the number of a specified type of tokens in the player's
+	 * inventory.
+	 * 
+	 * @param i
+	 *            The specified inventory to take from
+	 * @param num
+	 *            Number of tokens to take
+	 * 
+	 * @return Number of tokens taken
+	 */
 	public int addTokens(Inventory i, int num);
 
-	public int subTokens(Inventory i, int num);
+	/**
+	 * Decreases the number of a specified type of tokens in the player's
+	 * inventory.
+	 * 
+	 * @param i
+	 *            The specified inventory to give tokens to
+	 * @param num
+	 *            Number of tokens to give
+	 * 
+	 * @return Number of tokens given
+	 */
+	public int removeTokens(Inventory i, int num);
 
+	/**
+	 * Increases the number of a specified type of tokens in the player's
+	 * inventory. Generates report.
+	 * 
+	 * @param i
+	 *            The specified inventory to take from
+	 * @param num
+	 *            Number of tokens to take
+	 * 
+	 * @return Number of tokens taken
+	 */
 	public int addTokens_Report(Inventory i, int num);
 
-	public int subTokens_Report(Inventory i, int num);
+	/**
+	 * Decreases the number of a specified type of tokens in the player's
+	 * inventory. Generates report.
+	 * 
+	 * @param i
+	 *            The specified inventory to give tokens to
+	 * @param num
+	 *            Number of tokens to give
+	 * 
+	 * @return Number of tokens given
+	 */
+	public int removeTokens_Report(Inventory i, int num);
 }
