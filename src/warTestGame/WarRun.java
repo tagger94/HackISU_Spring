63eadb7f60b749c9dfad0ddad1f7c6f21949f677
Hawbaker <brennynh@iastate.edu>
2015-04-11 21:53:32 -0500

    Added reporter

--- src/warTestGame/WarRun.java
+++ src/warTestGame/WarRun.java
@@ -1,17 +1,24 @@
 package warTestGame;
 
+import gameSim.Reporter;
+
+/**
+ * Runs War games, currently prints statistics.
+ * 
+ * @author Brennyn Hawbaker
+ *
+ */
 public class WarRun {
 	
 	public static void main(String args[]){
-		for(int a = 0; a < 10000; a++){
+		for(int a = 0; a < 50; a++){
 			War game = new War();
 			game.playGame();
 		}
-		System.out.println("Player 1 won: " + War.get_p1W());
-		System.out.println("Player 2 won: " + War.get_p2W());
-		System.out.println("Average rounds per game: " + (War.totRounds*1.0)/War.games);
-		
-		System.out.println(War.get_p1W()*1.0/(War.get_p1W()+War.get_p2W()));
+		Reporter.printReport("Player 1 won: " + War.get_p1W());
+		Reporter.printReport("Player 2 won: " + War.get_p2W());
+		Reporter.printReport("Average rounds per game: " + (War.totRounds*1.0)/War.games);
+		Reporter.printReport((War.get_p1W()*1.0/(War.get_p1W()+War.get_p2W())*100) + "% games Player 1 won.");
 	}
 	
 }
